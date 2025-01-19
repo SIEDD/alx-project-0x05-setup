4daad27edfc284c75cc90b241c3a84a58ded6c6e
siedd <saidmwacharo8@gmail.com>
2025-01-19 22:30:02 +0300

    layout

--- alx-project-0x05/components/layouts/Header.tsx
+++ alx-project-0x05/components/layouts/Header.tsx
@@ -1,1 +1,33 @@
+import Link from "next/link";
+import Button from "../common/Button";
+import { usePathname } from "next/navigation";
+import { useCount } from "@/context/CountContext";
+
+const Header: React.FC = () => {
+  const pathname = usePathname();
+  const { count } = useCount();
+
+  return (
+    <header className="flex justify-between items-center px-6 py-4 bg-gradient-to-r from-green-400 to-blue-500 text-white">
+      {/* App Title */}
+      <Link href="/">
+        <a className="text-2xl font-bold">Splash App</a>
+      </Link>
+
+      {/* Button Group */}
+      <div className="flex gap-4">
+        {!["/counter-app"].includes(pathname) ? (
+          <>
+            <Button buttonLabel="Sign In" buttonBackgroundColor="red" />
+            <Button buttonLabel="Sign Up" buttonBackgroundColor="blue" />
+          </>
+        ) : (
+          <p className="font-semibold text-lg">Current count: {count}</p>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
 
